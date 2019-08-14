68aaf7fdc14119d61c7fb8aba9ff6effba4bbf1d
zoomout <bzastavnyi@gmail.com>
2019-08-14 23:24:03 +0200

    added events

--- src/index.js
+++ src/index.js
@@ -3,19 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+
     render() {
         return (
-            <button className="square">
-
+            <button className="square"
+                    onClick={() => this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+    }
 
-    renderSquare(i: number) {
-        return <Square/>;
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+        />;
     }
 
     render() {
@@ -41,6 +57,10 @@ class Board extends React.Component {
                 </div>
             </div>
         );
+    }
+
+    handleClick(i) {
+
     }
 }
 
